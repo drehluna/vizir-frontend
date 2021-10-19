76506a2f5f1212d61038100e8c091a7accf9496d
André Luna <drehluna@MacBook-Air-de-Andre.local>
2021-10-18 23:46:37 -0300

    Uploud project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { DropDownProvider } from './Contexts/DropDownContext';
+import { MultiSelectProvider } from './Contexts/MultiSelectContext';
+import './style.css'
 
 ReactDOM.render(
   <React.StrictMode>
+    <MultiSelectProvider>
+      <DropDownProvider>
     <App />
+    </DropDownProvider>
+    </MultiSelectProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
